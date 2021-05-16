b5cdeec23e788587e83ae3eab96910bc60faa11c
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-16 12:15:29 -0300

    ejercicio 25 (sin resolver)

---
+++ CAIGUIAFECHAS_EJR25/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIAFECHAS_EJR25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Le solicite al usuario el ingreso de tres números y determine si conforman una 
+            fecha válida interpretados como día / mes / año.*/
+
+            
+
+        }
+    }
+}
