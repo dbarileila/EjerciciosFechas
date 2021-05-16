ea86008b45bdca685ad2b61c5bd6b0329766e447
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-16 14:00:07 -0300

    ejercicio 30

---
+++ CAIGUIAFECHA_EJR30/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIAFECHA_EJR30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite el ingreso de una fecha al usuario y muestre sólo la hora.
+
+            do
+            {
+                Console.Write("Ingrese fecha (dd/MM/yyyy/hh): ");
+                string ingreso = Console.ReadLine();
+
+                if (!DateTime.TryParse(ingreso, out DateTime salida))
+                {
+                    Console.WriteLine("No ingreso une fecha correcta.");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Hora: {salida.Hour}:{salida.Minute}:{salida.Second}");
+                    break;
+                }
+
+            } while (true);
+            Console.ReadKey();
+
+        }
+    }
+}
