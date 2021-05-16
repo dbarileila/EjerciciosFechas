3d68dddb810097ce429e2215908a6b4278ea086d
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-16 10:45:40 -0300

    ejercicio 21

---
+++ CAIGUIAFECHAS_EJR21/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIAFECHAS_EJR21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Presente al usuario la fecha actual.
+
+            Console.WriteLine($"Fecha actual: {DateTime.Now}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
