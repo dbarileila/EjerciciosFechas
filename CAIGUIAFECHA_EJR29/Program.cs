78b4950718dfc04db09ee6eb9ba4a221d8863cfa
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-16 13:48:36 -0300

    ejercicio 29

---
+++ CAIGUIAFECHA_EJR29/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIAFECHA_EJR29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite el ingreso de una fecha al usuario y presente la fecha correspondiente 
+            //al primero de mes inmediato anterior.
+
+            do
+            {
+                Console.Write("Ingrese una fecha: ");
+                string ingreso = Console.ReadLine();
+
+                if (!DateTime.TryParse(ingreso, out DateTime salida))
+                {
+                    Console.WriteLine("No ingreso una fecha válida.");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Fecha al mes anterior: {salida.AddMonths(-1).ToShortDateString()}");
+                    break;
+                }
+
+            } while (true);
+            Console.ReadKey();
+        }
+    }
+}
