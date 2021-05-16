0ed928864ccc75e7f52a3200839ce7bfcf321619
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-16 11:21:21 -0300

    ejercicio 23

---
+++ CAIGUIAFECHAS_EJR23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIAFECHAS_EJR23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite el ingreso de una fecha al usuario y determine si es un ingreso válido.
+
+            do
+            {
+
+                Console.Write("Ingrese una fecha con el formato MM/dd/yyyy: ");
+
+                string ingreso = Console.ReadLine();
+
+                if (!DateTime.TryParse(ingreso, out DateTime salida))
+                {
+                    Console.WriteLine("No ingreso una fecha válida.");
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"Usted ingreso: {salida.ToShortDateString()}");
+                    break;
+                }
+
+            } while (true);
+
+            Console.ReadKey();
+        }
+    }
+}
