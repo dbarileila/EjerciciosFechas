d7f5ad7ae817a234bea5912e462125d26e4fcc8a
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-16 11:53:14 -0300

    ejercicio 24

---
+++ CAIGUIAFECHAS_EJR24/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CAIGUIAFECHAS_EJR24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite el ingreso de una fecha al usuario utilizando, estrictamente, el formato dd/MM/yyyy
+
+            do
+            {
+
+                Console.Write("Ingrese una fecha con el formato dd/MM/yyyy: ");
+                string ingreso = Console.ReadLine();
+                
+                if (!string.IsNullOrEmpty(ingreso))
+                {
+                    var dateTimeFormat = CultureInfo.InvariantCulture.DateTimeFormat;
+                    var date = DateTime.ParseExact(ingreso, "dd/MM/yyyy", dateTimeFormat);
+
+                    Console.WriteLine($"Usted ingreso: {ingreso}");
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Debe ingresar una fecha válida.");
+                    continue;
+                }
+
+
+            } while (true);
+
+            Console.ReadKey();
+        }
+    }
+}
