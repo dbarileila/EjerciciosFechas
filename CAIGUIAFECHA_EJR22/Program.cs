08bc7540cb73765455c556a6fb5d34aca57743bf
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-16 10:55:04 -0300

    ejercicio 22

---
+++ CAIGUIAFECHA_EJR22/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUIAFECHA_EJR22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Presente al usuario la fecha actual con el siguiente formato: dd/MM/yyyy
+
+            Console.WriteLine($" Fecha actual: {DateTime.Today.ToString("dd/MM/yyyy")}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
